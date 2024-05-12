4044b510af21257aab26ae9dbfea76008bca37ec
VincnetNgo12 <duchuy30092005@gmail.com>
2024-05-12 14:13:54 -0600

    Messing around with the contours

--- contours.js
+++ contours.js
@@ -2,12 +2,12 @@ function getContours(image){
     let src = cv.imread(image);
     let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
     cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 127, 255, cv.THRESH_BINARY);
+    cv.threshold(src, src, 100, 255, cv.THRESH_BINARY);
     // You can try more different parameters
-    //cv.Canny(src, dst, 50, 100, 3, false);
+    cv.Canny(src, dst, 50, 100, 3, false);
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
-    // You can try more different parameters
+    // You can try more different parameters        
     cv.findContours(src, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
 
 
