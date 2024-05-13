cf2f204f1323b18ea0c053db7c1dbc0a716e6df8
VincnetNgo12 <duchuy30092005@gmail.com>
2024-05-12 21:31:06 -0600

    Trials and Errors

--- contours.js
+++ contours.js
@@ -1,8 +1,14 @@
 function getContours(image){
     let src = cv.imread(image);
     let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+
+    // let ksize = new cv.Size(10, 10);
+    // let anchor = new cv.Point(-1, -1);
+    // // You can try more different parameters
+    // cv.blur(src, dst, ksize, anchor, cv.BORDER_DEFAULT);
+
     cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 127, 255, cv.THRESH_BINARY);
+    cv.threshold(src, src, 127, 255, cv.THRESH_OTSU);
     // You can try more different parameters
     //cv.Canny(src, dst, 50, 100, 3, false);
     let contours = new cv.MatVector();
