7c756244bba19e7c42244954f2446213dd52db82
VincnetNgo12 <duchuy30092005@gmail.com>
2024-05-23 17:13:44 -0600

    Filter Contours Based on Area

--- contours.js
+++ contours.js
@@ -2,19 +2,6 @@ function getContours(image){
     let src = cv.imread(image);
     let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
 
-    // // Pre-processing: Noise reduction with opening
-    // let kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(3, 3));
-    // cv.morphologyEx(src, src, cv.MORPH_OPEN, kernel);
-
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(3,3));
-
-    // MORPH_OPEN = removes noise (erosion then dilation)
-    cv.morphologyEx(src, src, cv.MORPH_OPEN, kernel);
-
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(2,2));
-
-    // MORPH_CLOSE - closes small holes inside the foreground objects, or small black points on the object (dilation then erosion)
-    cv.morphologyEx(src, src, cv.MORPH_CLOSE, kernel); 
 
     cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0); //Set the color scheme 
 
@@ -26,7 +13,7 @@ function getContours(image){
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
     // You can try more different parameters        
-    cv.findContours(src, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_NONE );
+    cv.findContours(src, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
 
 
     let contourPoints = getContourCoordinates(contours);
@@ -41,12 +28,16 @@ function getContourCoordinates(contours) {
     let points = {};
     for (let i = 0; i < contours.size(); ++i) {
         const ci = contours.get(i); //current contour
-        points[i] = [];
-        for (let j = 0; j < ci.data32S.length; j += 2){
-        let p = {}
-        p.x = ci.data32S[j]
-        p.y = ci.data32S[j+1]
-        points[i].push(p)
+        let contourArea = cv.contourArea(ci); //current contour area
+
+        if(contourArea > 10){ //filter out small contours
+            points[i] = [];
+            for (let j = 0; j < ci.data32S.length; j += 2){
+                let p = {}
+                p.x = ci.data32S[j]
+                p.y = ci.data32S[j+1]
+                points[i].push(p)
+            }
         }
     }
 
